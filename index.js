2239a9c34039027d3e23cfc5564cc17baf86e111
zahidhasan125 <zahidhasan125@gmail.com>
2022-11-24 03:22:56 +0600

    Server is running

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Power Tools Buy-Sell Server is running.')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+})
